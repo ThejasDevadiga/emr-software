7728614fb8816fa3a5e04f41c156f918f27a0e7e
ThejasDevadiga <4al20ai047@gmail.com>
2022-12-03 22:42:44 +0530

    3-12

--- emrFrontend/LabTechnician/JS/index.js
+++ emrFrontend/LabTechnician/JS/index.js
@@ -4,3 +4,14 @@ fetch('../sideBar.html')
 .then(data =>{
     sideBar.innerHTML = data;
 })
+
+
+
+const navbar = document.querySelector("nav")
+fetch('../../Navbar/navbar.html')
+.then(res=>res.text())
+.then(data =>{
+    navbar.innerHTML = data;
+})
+
+
